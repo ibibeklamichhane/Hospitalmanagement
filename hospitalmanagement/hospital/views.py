4162288ead8fd6d11f1ddb2d60e64bc309acf894
ibibeklamichhane <bibeklc714@gmail.com>
2021-06-07 14:14:25 +0545

    aded navlinks

--- hospitalmanagement/hospital/views.py
+++ hospitalmanagement/hospital/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
      return render(request, 'hospital/home.html', {})
+
+def about(request):
+     return render(request, 'hospital/about.html', {})
+
+def contact(request):
+     return render(request, 'hospital/contact.html', {})
