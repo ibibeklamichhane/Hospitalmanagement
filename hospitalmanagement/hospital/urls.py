0efe08fea1263bc9ec8332d17c9e0d1f35f8db4e
ibibeklamichhane <bibeklc714@gmail.com>
2021-06-26 00:20:53 +0545

    created login and logout page

--- hospitalmanagement/hospital/urls.py
+++ hospitalmanagement/hospital/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='hospital/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='hospital/logout.html'), name='logout'),
     
 ]
 
--- hospitalmanagement/hospital/views.py
+++ hospitalmanagement/hospital/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import Register
-
+from django.contrib.auth import authenticate,login,logout
 def home(request):
      return render(request, 'hospital/home.html', {})
 
@@ -19,7 +19,25 @@ def register(request):
 			print("HELLO")
 			print(form)
 			form.save()
-			return redirect('home')
+			return redirect('login')
 	else:
 		form = Register()
 	return render(request,'hospital/register.html',{'form':form})
+
+
+def Login(request):
+	error = ""
+	if request.method=='POST':
+		u = request.POST['uname']
+		p = request.POST['pwd']
+		user = authenticate(username=u,password=p)
+		try:
+			if user.is_staff:
+				login(request,user)
+				error = "yes"
+			else:
+				error = "no"
+		except:
+			error = "no"
+	d = {'error':error}
+	return render(request,'hospital/login.html',d)
